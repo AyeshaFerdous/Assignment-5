b9569cb0a6934347af1ba6635ea93f756c0aede1
AyeshaFerdous <ayeshaferdous01@gmail.com>
2024-09-23 02:20:58 +0600

    added a card section and blog button functionality

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function blogBtnClick(){
+   let url = "http://127.0.0.1:5500/blog.html";
+   window.location.assign(url);
+}
+
+function homeBtnClick(){
+    let url = "http://127.0.0.1:5500/index.html";
+    window.location.assign(url);
+ }
