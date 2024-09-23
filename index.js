eee4761997e11ec83dd3de13b98e74609a371397
AyeshaFerdous <ayeshaferdous01@gmail.com>
2024-09-23 22:12:52 +0600

    change some code

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 
-function blogBtnClick(){
-   let url = "http://127.0.0.1:5500/blog.html";
-   window.location.assign(url);
-}
-
-function homeBtnClick(){
-    let url = "http://127.0.0.1:5500/index.html";
-    window.location.assign(url);
- }
 
 //  noakhali donate button
 
 document.getElementById('noakhali-donate-btn').addEventListener('click', function(){
-
-   
    const noakhaliAmount = parseFloat(document.getElementById('noakhali-amount').value);
 
    if(noakhaliAmount < 0 || isNaN(noakhaliAmount)){
